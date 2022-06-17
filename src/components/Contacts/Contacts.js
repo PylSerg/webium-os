dc1b3dc323529291341fce7bc4a97286d5eaf10e
Sergei <pylypenko.sn@gmail.com>
2022-06-17 16:00:43 +0300

    adds close button

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -2,5 +2,5 @@ import React from "react";
 import styles from "./Contacts.module.css";
 
 export default function Contacts() {
-	return <div className={styles.container}>dfhfjkkjk ljlkjljlkjlk jlk jl jl jljkljlk jlkj ljkhhg</div>;
+	return <>jhjkhjjlkhhj g gjjhk jhkj</>;
 }
--- src/components/Dockbar/Dockbar.js
+++ src/components/Dockbar/Dockbar.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import favorites from "./favorites";
 import Contacts from "../Contacts";
 import styles from "./Dockbar.module.css";
+import stylesContacts from "../Contacts/Contacts.module.css";
 
 export default function Dockbar() {
 	const [state, setState] = useState(favorites.favorites);
@@ -13,9 +14,21 @@ export default function Dockbar() {
 		setState([...state, (state[idx].open = true)]);
 	};
 
+	const closeWindow = idx => {
+		setState([...state, (state[idx].open = false)]);
+	};
+
 	return (
 		<div>
-			{state[0].open && <Contacts />}
+			{state[0].open && (
+				<div className={stylesContacts.container}>
+					<div className={stylesContacts.header}>
+						<button type="button" className={stylesContacts.close} onClick={() => closeWindow(0)} title="Close"></button>
+					</div>
+
+					<Contacts />
+				</div>
+			)}
 			<ul className={styles.container}>
 				{state.map((f, index) => (
 					<li className={styles.item} key={f.id} onClick={() => openWindow(index)}>
