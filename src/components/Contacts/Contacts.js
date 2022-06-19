bffa349cad804f881d40cea78d85a0979062c8a3
Sergei <pylypenko.sn@gmail.com>
2022-06-19 13:16:50 +0300

    adds contacts list

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import ContactsList from "./ContactsList";
 import ContactView from "./ContactView";
+import contactsDB from "./contactsDB.json";
 import styles from "./Contacts.module.css";
 
 export default function Contacts() {
+	const [contacts, setContacts] = useState(contactsDB);
+
+	console.table(contacts);
+
+	function viewContact(contId) {
+		setContacts(prevContacts =>
+			prevContacts.map(contact => {
+				if (contact.id === contId) return { ...contact, view: true };
+				return contact;
+			})
+		);
+	}
+
 	return (
 		<div className={styles.container}>
-			<ContactsList />
-			<ContactView />
+			<div className={styles.sidebar}>
+				<ContactsList contacts={contacts} viewContact={viewContact} />
+			</div>
+			<ContactView contacts={contacts} />
 		</div>
 	);
 }
--- src/components/Contacts/ContactsList.js
+++ src/components/Contacts/ContactsList.js
@@ -1,6 +1,14 @@
 import React from "react";
 import styles from "./Contacts.module.css";
 
-export default function ContactView() {
-	return <div className={styles.list}>hjgjhghgjg</div>;
+export default function ContactView({ contacts, viewContact }) {
+	return (
+		<ul className={styles.list}>
+			{contacts.map(contact => (
+				<li className={styles.item} key={contact.id} onClick={() => viewContact(contact.id)}>
+					{contact.name} {contact.lastName}
+				</li>
+			))}
+		</ul>
+	);
 }
