3a666d56a47c6f23ba9497fa64be2cafa180e70c
Sergei <pylypenko.sn@gmail.com>
2022-06-29 19:57:55 +0300

    deletes HomePage

--- src/components/Music/Navigation/Navigation.js
+++ src/components/Music/Navigation/Navigation.js
@@ -3,7 +3,6 @@ import { NavLink } from "react-router-dom";
 export default function Navigation() {
 	return (
 		<nav>
-			{/* <NavLink to="/">Home</NavLink> */}
 			<NavLink to="/playlist-1">Playlist 1</NavLink>
 			<NavLink to="/playlist-2">Playlist 2</NavLink>
 			<NavLink to="/playlist-3">Playlist 3</NavLink>
